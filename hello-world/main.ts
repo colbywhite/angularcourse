b2bd870fe5fbbd4f389a09f354759b69d2a45871
Colby M. White <blackbaud-colbywhite@users.noreply.github.com>
2018-10-17 18:59:26 -0500

    vocab regarding field/property/accessors

--- hello-world/main.ts
+++ hello-world/main.ts
@@ -14,3 +14,39 @@ function doSomething() {
 }
 
 doSomething();
+
+
+class Point {
+  // _x and _y are fields
+  constructor(private _x: number, private _y: number) {
+  }
+
+  // x is a property, i.e. a field that uses an accessor method
+  public get x() {
+    return this._x;
+  }
+
+  public set x(value: number) {
+    if (value < 0) {
+      throw new Error('Coordinate cannot be less than zero');
+    }
+    this._x = value;
+  }
+
+  // y is a property, i.e. a field that uses an accessor method
+  public get y() {
+    return this._y;
+  }
+
+  public set y(value: number) {
+    if (value < 0) {
+      throw new Error('Coordinate cannot be less than zero');
+    }
+    this._y = value;
+  }
+
+}
+
+let point = new Point(1, 4);
+// from the outside, properties look like fields, but they're slight different
+console.log('x', point.x, 'y', point.y);
